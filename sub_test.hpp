31654910bdde39cbb4beafcf347a00c13a313b44
cheyenneayyoub <cayyo001@ucr.edu>
2021-04-19 23:24:11 -0700

    fixed string errors

--- sub_test.hpp
+++ sub_test.hpp
@@ -37,21 +37,21 @@ TEST(SubTest, SubStringPos){
         Op* val1 = new Op(2.8);
         Op* val2 = new Op(1.0);
         Sub* test = new Sub(val1, val2);
-        EXPECT_EQ(test->stringify(), "1.800000");
+        EXPECT_EQ(test->stringify(), "2.800000-1.000000");
 }
 
 TEST(SubTest, SubStringNeg){
         Op* val1 = new Op(-2.8);
         Op* val2 = new Op(-1.0);
         Sub* test = new Sub(val1, val2);
-        EXPECT_EQ(test->stringify(), "-3.800000");
+        EXPECT_EQ(test->stringify(), "(-2.800000--1.000000)");
 } 
 
 TEST(SubTest, SubStringZero){
         Op* val1 = new Op(0.0);
         Op* val2 = new Op(0.0);
         Sub* test = new Sub(val1, val2);
-        EXPECT_EQ(test->stringify(), "0.000000");
+        EXPECT_EQ(test->stringify(), "0.000000-0.000000");
 }
 
 TEST(SubTest, SubStringAdd){
