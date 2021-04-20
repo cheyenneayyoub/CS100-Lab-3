1075bd5b0743ed146fac2a393ec5101809324352
cheyenneayyoub <cayyo001@ucr.edu>
2021-04-20 02:34:54 -0700

    Finalized CMakeList.txt and main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
 #include "base.hpp"
+#include "add.hpp"
+#include "sub.hpp"
+#include "pow.hpp"
+#include "mult.hpp"
+#include "div.hpp"
+#include "op.hpp"
+#include "rand.hpp"
 
 int main() {
     // This is a very basic main, and being able to correctly execute this main
