26859309388015fc29c556b8f31260f9eb481943
github-shwenakak <github-shwenakak@gmail.com>
2021-04-20 00:11:06 -0700

    Added my classes to unit

--- unit_test.cpp
+++ unit_test.cpp
@@ -1,6 +1,10 @@
 #include "gtest/gtest.h"
 
 #include "op_test.hpp"
+#include "randtest.hpp"
+#include "multtest.hpp"
+#include "divtest.hpp"
+
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
