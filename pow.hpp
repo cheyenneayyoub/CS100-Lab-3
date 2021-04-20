53fb019c7e3b4eb582289389854826137eef819c
cheyenneayyoub <cayyo001@ucr.edu>
2021-04-20 01:46:37 -0700

    refined the throw

--- pow.hpp
+++ pow.hpp
@@ -13,6 +13,8 @@ class Pow : public Base{
 		Base* rhs;
 	public:
 		Pow(Base* left, Base* right) : Base() {
+			if(left==nullptr || right == nullptr){throw std::invalid_argument("Null argument passed into Pow.");}
+                        if(left->evaluate() < 0 && (right->evaluate()<1)){throw std::invalid_argument("Imaginary number.");}
 			lhs = left;
 			rhs = right;
 		}
