5d460b566660e4c9fa5374a31495743be32dbafd
cheyenneayyoub <cayyo001@ucr.edu>
2021-04-20 01:14:18 -0700

    fixed some errors

--- pow_test.hpp
+++ pow_test.hpp
@@ -28,21 +28,21 @@ TEST(PowTest, NoArgument){
 TEST(PowTest, PowEvalPos){
 	Base* val1 = new Op(3.0);
 	Base* val2 = new Op(2.0);
-	Pow* test = new Pow(val1,val2);
+	Base* test = new Pow(val1,val2);
 	EXPECT_EQ(test->evaluate(), 9.0);
 }
 
 TEST(PowTest, PowEvalNegBase){
         Base* val1 = new Op(-3.0);
         Base* val2 = new Op(2.0);
-        Pow* test = new Pow(val1,val2);
+        Base* test = new Pow(val1,val2);
         EXPECT_EQ(test->evaluate(), 9.0);
 }
 
 TEST(PowTest, PowEvalFractionPow){
         Base* val1 = new Op(9.0);
         Base* val2 = new Op(0.5);
-        Pow* test = new Pow(val1,val2);
+        Base* test = new Pow(val1,val2);
         EXPECT_EQ(test->evaluate(), 3.0);
 }
 
@@ -52,21 +52,21 @@ TEST(PowTest, PowEvalAddSub){
 	Base* val3 = new Op(3.0);
 	Add* sum = new Add(val1,val2);
 	Sub* sub = new Sub(sum,val3);
-	Pow* test = new Pow(sub,val1);
+	Base* test = new Pow(sub,val1);
         EXPECT_EQ(test->evaluate(), 1.0);
 }
 
 TEST(PowTest, PowStrPos){
         Base* val1 = new Op(3.0);
         Base* val2 = new Op(2.0);
-        Pow* test = new Pow(val1,val2);
+        Base* test = new Pow(val1,val2);
         EXPECT_EQ(test->stringify(), "(3.000000**2.000000)");
 }
 
 TEST(PowTest, PowStrFractionPow){
         Base* val1 = new Op(9.0);
         Base* val2 = new Op(0.5);
-        Pow* test = new Pow(val1,val2);
+        Base* test = new Pow(val1,val2);
         EXPECT_EQ(test->stringify(), "(9.000000**0.500000)");
 }
 
@@ -76,6 +76,6 @@ TEST(PowTest, PowStrAddSub){
         Base* val3 = new Op(3.0);
         Add* sum = new Add(val1,val2);
         Sub* sub = new Sub(sum,val3);
-        Pow* test = new Pow(sub,val1);
+        Base* test = new Pow(sub,val1);
         EXPECT_EQ(test->stringify(), "(((2.000000+2.000000)-3.000000)**2.000000)");
 }
