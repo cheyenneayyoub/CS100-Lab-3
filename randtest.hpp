c200786cf22976470559c0434d0423f54a1311b3
github-shwenakak <github-shwenakak@gmail.com>
2021-04-19 23:02:58 -0700

    These are my testcases for Rand

---
+++ randtest.hpp
@@ -0,0 +1,28 @@
+#ifndef _RANDTEST_HPP_
+#define _RANDTEST_HPP
+
+#include "gtest/gtest.h"
+#include "base.hpp"
+#include "rand.hpp"
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+TEST(RandTest, RandCheckLE) {
+	Rand* test = new Rand ();
+	EXPECT_LE(test->evaluate(), 100)
+}
+
+TEST(RandTest, RandCheckGE) {
+        Rand* test = new Rand ();
+        EXPECT_GE(test->evaluate(), 0)
+}  
+
+TEST(RandTest, RandCheckString) {
+        Rand* test = new Rand ();
+        EXPECT_NE(test->evaluate(), -1)
+}
+
+
